c5d999bdac4a500f8988bc7c5895568f634f72fc
Darcie Murphy <Murphy-D72@ulster.ac.uk>
2023-11-22 11:42:23 +0000

    Add project files.

---
+++ validSentenceCheck/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace validSentenceCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
